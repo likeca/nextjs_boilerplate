69d636af9c72bc319629c5a108c4300537101802
likeca <like.ca@gmail.com>
2026-06-14 13:54:01 -0400

    fix recharts

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -114,6 +114,8 @@ function ChartTooltipContent({
     indicator?: 'line' | 'dot' | 'dashed';
     nameKey?: string;
     labelKey?: string;
+    payload?: any[];
+    label?: any;
   }) {
   const { config } = useChart();
 
@@ -155,8 +157,8 @@ function ChartTooltipContent({
       {!nestLabel ? tooltipLabel : null}
       <div className="grid gap-1.5">
         {payload
-          .filter((item: any) => item.type !== 'none')
-          .map((item: any, index: number) => {
+          .filter((item) => item.type !== 'none')
+          .map((item, index) => {
             const key = `${nameKey || item.name || item.dataKey || 'value'}`;
             const itemConfig = getPayloadConfigFromPayload(config, item, key);
             const indicatorColor = color || item.payload.fill || item.color;
