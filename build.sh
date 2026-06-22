67fff705c9c19f97f71a04fba36c4ef0a90b132d
likeca <like.ca@gmail.com>
2026-06-21 23:13:00 -0400

    update action

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo -e "======== Docker build ========"
 docker build -t likeca/nextjs . -f Dockerfile
-# docker push likeca/nextjs:latest
+docker push likeca/nextjs:latest
 echo -e "======== End of docker build ========\n"
 
 echo -e "======== Docker compose up ========"
-docker start nextjs
+# docker start nextjs
 # docker compose up -d
 echo -e "======== End of docker compose up ========\n"
 
