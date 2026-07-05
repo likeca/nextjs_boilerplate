020136e9eb5e878444b928c95180b998a4cb7a80
likeca <like.ca@gmail.com>
2026-07-05 19:22:50 -0400

    google recaptcha csp

--- next.config.ts
+++ next.config.ts
@@ -49,10 +49,10 @@ const nextConfig: NextConfig = {
                 // Cloudflare Insights + Google reCAPTCHA
                 "connect-src 'self' https://cloudflareinsights.com https://www.google.com",
                 "script-src 'self' 'unsafe-eval' 'unsafe-inline' 'https://static.cloudflareinsights.com' 'https://www.google.com' 'https://www.gstatic.com'",
+                "frame-src 'self' https://www.google.com",
 
                 // "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.cloudflareinsights.com https://www.google.com https://www.gstatic.com",
                 // // Google reCAPTCHA renders its challenge/badge in an iframe from www.google.com
-                // "frame-src 'self' https://www.google.com",
 
                 // "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
                 "style-src 'self' 'unsafe-inline'",
