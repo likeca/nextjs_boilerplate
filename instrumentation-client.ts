137c62bfc27b9c66bc1e9253377ca75c1799d183
likeca <like.ca@gmail.com>
2026-07-05 08:18:36 -0400

    disable posthog header

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,6 @@ if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2026-05-30',
-    opt_out_capturing_by_default: true, // wait for cookie consent
+    // opt_out_capturing_by_default: true, // Wait for cookie consent before capture
   });
 }
